0a850e02e8cdcc05788f711e903c9016d351d1bf
Deja Ware <d.ware9873@gmail.com>
2026-02-08 21:54:15 -0600

    Remove unneed Flare directory after restructure

--- Flare/ml/brain/ct/train_ct.py
+++
@@ -1,9 +0,0 @@
-from torchvision import transforms 
-
-transforms = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
-    transforms.Resize((256, 256)), 
-    transforms.ToTensor(), 
-    transforms.Normalize((0.5), (0.5)) # grayscale channel normalization
-])
-
